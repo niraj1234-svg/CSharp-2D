e10cea8865ec971be4227ac7e402cbcc5c80e264
testing <dhoreniraj83@gmail.com>
2026-04-29 18:15:00 +0530

    player movement

---
+++ Assets/Escape Room 2D/Leaning/Scripts/Playermovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private bool isGrounded = false;
+    public  float jumpForce = 7f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
